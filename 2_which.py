5d99fa318533351bb95fe620a52017a619c07289
Aaron Aguerrevere <aaguerrevere@legacy.com>
2021-01-22 13:37:34 -0600

    sending result from which.py to destination path

--- 2_which.py
+++ 2_which.py
@@ -1,9 +1,14 @@
 ###--- IMPORTS ---###
 import settings
 import csv
+import os
 
 
 ###--- GLOBAL VARIABLES ---###
+parser_path = settings.PARSER_PATH
+
+destination_path = settings.DESTINATION_PATH
+
 more_standard_patterns_list = [i for i in settings.more_standard_patterns.splitlines() if i != '']
 
 
@@ -13,14 +18,12 @@ def create_csv_which_parsers_contain_which_patterns():
     with open('TEST_parser.pl', 'r', encoding="utf8") as parser:
         reader = parser.read()
 
-    with open('which.csv', 'w', newline='') as csvfile:
+    with open(os.path.join(destination_path, 'which.csv'), 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['pattern', 'found'])
         writer.writeheader()
 
         for pattern in more_standard_patterns_list:
             writer.writerows([{'pattern': pattern, 'found': pattern in reader}])
-
-    # print(f"\n\n  {count} out of {len(more_standard_patterns_list)} patters found in this parser")
 
 
 ###--- DRIVER CODE ---###
